624d0fb84b9cc7131a02ea3bc8bd0d09fa5b6ef3
HBTNQ <hoangthai0205@gmail.com>
2024-12-21 14:33:12 +0700

    fix file main\index.jsx

--- src/page/admin/component/main/index.jsx
+++ src/page/admin/component/main/index.jsx
@@ -6,14 +6,12 @@ import App from "../footer/App";
 
 
 const AdminMain = ({ children, ...props }) => {
-  return (
-    <div {...props}>
-      <div className="main-content">
-        <Header />
-        <Sidebar />
-        {children}
-        <Footer />
-      </div>
+   return (
+    <div>
+      {children}
+      <Footer />
+    </div>
   );
+
 };
 export default memo(AdminMain);
